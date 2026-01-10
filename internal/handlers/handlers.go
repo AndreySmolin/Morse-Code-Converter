3db5755e092d249e0d64b4f5615ba45de1ba3e52
Andrey <saselitasm@yandex.ru>
2026-01-10 17:20:10 +0300

    first commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,53 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusInternalServerError)
+		return
+	}
+	http.ServeFile(w, r, "../index.html")
+}
+func LoadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusInternalServerError)
+		return
+	}
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Cannot parse form", http.StatusInternalServerError)
+		return
+	}
+	file, _, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "No file uploaded", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	line, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "reading error", http.StatusInternalServerError)
+		return
+	}
+	str, err := service.DetectionMorse(string(line))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
+	}
+	name := fmt.Sprintf("%s.txt", time.Now().UTC().String())
+	if err := os.WriteFile(name, []byte(str), 0755); err != nil {
+		http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Disposition", `inline; filename="`+name+`"`)
+	http.ServeFile(w, r, name)
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,19 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func DetectionMorse(data string) (string, error) {
+	sliceData := strings.Split(data, "")
+	if len(sliceData) == 0 {
+		return "", errors.New("there is no data to display")
+	}
+	if sliceData[0] == "." || sliceData[0] == "-" {
+		return morse.ToText(data), nil
+	}
+	return morse.ToMorse(data), nil
+}
